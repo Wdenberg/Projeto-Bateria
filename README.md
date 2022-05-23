d1ea0d0eb85cc173b06c3d91539298bce65b9ee7
Wdenberg <wdenbergskillshd@gmail.com>
2022-05-23 11:06:55 -0300

    Projeto Bateria Musical

---
+++ README.md
@@ -0,0 +1,2 @@
+Projeto simples de uma Bateria em Html, Css mais JavaScript
+Desenvolvido por Wdenberg Ramos
---
+++ script.js
@@ -0,0 +1,45 @@
+document.body.addEventListener('keyup',(event)=>{
+  palySound(event.code.toLowerCase());
+});
+
+document.querySelector('.composer button').addEventListener('click', ()=>{
+  let song = document.querySelector('#input').value;
+
+  if(song !== ''){
+    let songArray = song.split('');
+    playComposition(songArray);
+  }
+});
+
+function palySound(sound){
+  let audioElement = document.querySelector(`#s_${sound}`);
+  let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+  if(audioElement){
+    audioElement.play();
+  }
+
+
+
+  if(keyElement){
+    audioElement.currentTime = 0;
+    keyElement.classList.add('active');
+
+    setTimeout(()=>{
+      keyElement.classList.remove('active');
+    },300);
+  }
+}
+
+function playComposition(songArray){
+
+  let wait = 0;
+
+  for(let songItem of songArray){
+    setTimeout(()=>{
+      palySound(`key${songItem}`);
+    }, wait);
+
+    wait += 250;
+    
+  }
+}
